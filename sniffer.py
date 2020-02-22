532cba499a1320642fabcd0980d040287c3f1e48
khan-ibrahim <5855449+khan-ibrahim@users.noreply.github.com>
2020-02-22 12:50:37 -0500

    added live sniffing and read from pcap support.\n Enhanced arg parsing.

--- sniffer.py
+++ sniffer.py
@@ -3,18 +3,23 @@
 
 import argparse
 import socket
+from scapy.all import *
 
 
 def parseFromFile(pcapFile, bpf):
-    print('reading from file {}'.format(pcapFile))
+    print('reading from file {}\n'.format(pcapFile))
+    sniff(prn=evaluatePacket, filter=bpf, offline=pcapFile, store=0)
     return 0
 
 def sniffLive(interfaceName, bpf):
-    print('sniffing live on interface {}'.format(interfaceName))
+    print('sniffing live on interface {}\n'.format(interfaceName))
+    sniff(prn=evaluatePacket, filter=bpf, iface=interfaceName, store=0)
     return 0 
 
 def evaluatePacket(pkt):
-    return 0
+    #print('evaluating packet {}'.format(pkt)) 
+    print(pkt.summary())
+    return
 
 def main():
     print('Starting sniffer.py')
@@ -22,19 +27,22 @@ def main():
     #parse args
     # sniffer.py [-i interface] [-r tracefile] [-e expression]
     parser = argparse.ArgumentParser()
-    
     whereToReadFrom = parser.add_mutually_exclusive_group()
 
-    whereToReadFrom.add_argument('-i', nargs='?', const='*', choices=[x[1] for x in socket.if_nameindex()]+['*'], metavar='interfaceName', help='specify interface to sniff packets on, default *.')
-
+    whereToReadFrom.add_argument('-i', nargs='?', \
+            choices=[x[1] for x in socket.if_nameindex()]+['*'], metavar='interfaceName', \
+            help='specify interface to sniff packets on, default *.')
     
     whereToReadFrom.add_argument('-r', nargs=1, metavar='file.cap', help='specify pcap to read from')
 
     parser.add_argument('-e', nargs=1, metavar='BPF', help='specify BPF expression')
     
     parsed = parser.parse_args()
+    print(parsed)
 
-    #print(parsed)
+    load_layer('http')
+    load_layer('tls')
+    
     retval = 0
 
     if not parsed.r == None:
